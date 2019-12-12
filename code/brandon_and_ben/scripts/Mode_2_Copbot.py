0dda3480d34b09a52df7a603a5f0e97bf9a6dd47
Bandreu1006 <54557940+Bandreu1006@users.noreply.github.com>
2019-12-12 17:25:20 -0500

    Update Mode_2_Copbot.py

--- code/brandon_and_ben/scripts/Mode_2_Copbot.py
+++ code/brandon_and_ben/scripts/Mode_2_Copbot.py
@@ -10,7 +10,7 @@ class Finder:
 		cv2.namedWindow("window", 1)
 		self.image_sub = rospy.Subscriber('/robot1/camera/rgb/image_raw', Image, self.image_callback)
 		self.state = 1
-		print ('\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n Catch the criminal!')
+		print ('\n \n \n \n \n \n Catch the criminal!')
 		
 	def image_callback(self, msg):
 		model_coordinates = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
